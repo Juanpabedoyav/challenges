95452e324674458a177ecb5afed19e356f1f16a2
jbedoyav <jbedoyav@grupo-exito.com>
2024-05-23 00:32:49 -0500

    📦 NEW: render list

--- lib/features/shares/presentation/pages/shares_screen.dart
+++ lib/features/shares/presentation/pages/shares_screen.dart
@@ -1,4 +1,5 @@
 import 'package:challenge1/features/shares/presentation/bloc/shares/shares_bloc.dart';
+import 'package:challenge1/features/shares/presentation/bloc/shares/shares_event.dart';
 import 'package:challenge1/features/shares/presentation/bloc/shares/shares_state.dart';
 import 'package:challenge1/features/shares/presentation/widgets/shares_list.dart';
 import 'package:flutter/material.dart';
@@ -10,16 +11,21 @@ class SharesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shares'),
+      ),
       body: BlocBuilder<SharesBloc, SharesState>(
         builder: (context, state) {
           if (state is FetchSharesLoading) {
             return const CircularProgressIndicator();
           } else if (state is FetchSharesLoaded) {
+            print('cargé');
             return SharesList(sharesData: state.shares);
           } else if (state is FetchSharesError) {
+            print('error');
             return Text(state.error);
           }
-          return Container();
+          return Text('error');
         },
       ),
     );
